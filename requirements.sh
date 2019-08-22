39e49ac89f84064abdabed70cda26f98093c6d6d
VioletaMisheva <violeta_misheva@yahoo.com>
2019-08-22 15:32:56 +0200

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+# Install specific package versions with pip
+# I used the following packages to execute the code
+
+pip3 install pandas == 0.23.4
+pip3 install numpy == 1.16.4
+pip3 install seaborn == 0.9.0
+pip3 install matplotlib == 2.2.3
+pip3 install sklearn == 0.21.2
+pip3 install pycebox == 0.0.1
+pip3 install eli5 == 0.9.9
+pip3 install lime ==0.1.1.36
+pip3 install shap == 0.26.0
